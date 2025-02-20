3c878e22958cf2497e56cc60b032f4101ad38826
ninjacoder-ui <coderninja200@gmail.com>
2025-02-20 20:32:58 +0530

    Update blockchainproject_d.py

--- blockchainproject_d.py
+++ blockchainproject_d.py
@@ -51,7 +51,7 @@ txnBuffer = [makeTransaction() for i in range(30)]
 # WAIT WAIT Project Code is longer and detailed explained.
 # Mail me to get Full Project Code at [ vatshayan007@gmail.com ]
 
-"""# Mail me at vatshayan007@gail.com for Project Files.
+"""# Mail me at coderninja200@gmail.com for Project Files.
 ## Project Files will include Project Code, Project PPT. Report and documentations 
 """
 
